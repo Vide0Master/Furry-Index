a42379da53369ce969e411a928646e2cbef6f4c7
Vide0Master <signatov2134@gmail.com>
2025-07-17 12:10:53 +0300

    automatic language select based on current client language

--- webpage/scripts/language.js
+++ webpage/scripts/language.js
@@ -8,19 +8,32 @@ const languages = {
     RU
 }
 
-export default class Language {
+class Language {
     static availableLanguages = Object.keys(languages)
 
     static currentLang = localStorage.getItem('language') || 'ENG'
 
     static lang = languages[this.currentLang]
 
-    static setLanguage(lang) {
+    static setLanguage(lang, reload = false) {
         if (!this.availableLanguages.includes(lang)) return
         localStorage.setItem('language', lang)
         this.currentLang = lang
         this.lang = languages[this.currentLang]
 
-        location.reload();
+        if (reload) location.reload();
     }
 }
+
+const browserLanguage = navigator.language.split('-')[0]
+
+const langsInternal = {
+    en: 'ENG',
+    ru: 'RU',
+    ua: 'UA'
+}
+
+if (Object.keys(langsInternal).includes(browserLanguage) && !localStorage.getItem('language'))
+    Language.setLanguage(langsInternal[browserLanguage], false)
+
+export default Language
