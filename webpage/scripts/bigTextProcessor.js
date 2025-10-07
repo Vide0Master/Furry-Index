72932034502126ba96b629e6db2b9cada964677f
Vide0Master <signatov2134@gmail.com>
2025-10-07 14:24:13 +0300

    backgrounds for TOS and PP

--- webpage/scripts/bigTextProcessor.js
+++ webpage/scripts/bigTextProcessor.js
@@ -1,7 +1,7 @@
 import Elem from "../components/elem/script.js"
 
 export default function processText(text, parent) {
-    const elem = new Elem("reg-info-cont", parent)
+    const elem = new Elem("BT-info-cont", parent)
     for (const textB of text) {
         const block = new Elem("info-block", elem.element)
 
